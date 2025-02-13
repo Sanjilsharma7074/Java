63ac01b839bc74e6d1a826d0d392bb33e766d047
Sanjilsharma7074 <sanjilsharma456@gmail.com>
2025-02-13 15:39:00 +0530

    Ceiling

---
+++ Binary_Search/code/Ceiling.java
@@ -0,0 +1,43 @@
+package code;
+import java.util.Scanner;
+public class Ceiling {
+    static int ceiling(int[] arr, int upper){
+        int start = 0;
+        int end = arr.length-1;
+        
+        while(start<=end){
+        int mid = start+(end-start)/2;
+          if (arr[mid]==upper){
+            return mid;
+          }
+          else if(arr[mid]<upper){
+        
+            start = mid + 1;
+          }
+          else {
+              end = mid - 1;
+          }
+        }
+  
+        return start;
+    }
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Please enter the length you want for your array: ");
+        int len = in.nextInt();
+        int[] arr = new int[len];
+
+        for(int i=0 ;  i<arr.length ; i++){
+            arr[i] = in.nextInt();
+        }
+        for(int i=0 ;  i<arr.length ; i++){
+            System.out.print(arr[i]+" ");
+        }
+        System.out.println();
+        System.out.print("Please enter the number whose smallest >= (greater than equal to) number you want to find: ");
+        int target = in.nextInt();
+        int ans = ceiling(arr, target);
+        System.out.println(ans);
+        in.close();
+    }
+}
